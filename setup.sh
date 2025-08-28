b0d1d42f147a3bbdf06412ecde9a0ba9090c60a2
xmasterpiecex <a211019971420125@gmail.com>
2025-08-28 19:13:46 +0300

    minor changes

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+python3 -m venv ./.venv
+
+source .venv/bin/activate
+
+pip install -r require.txt
--- voiceChange.sh
+++ voiceChange.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-FULL_PATH="$HOME/forfun/probe/pythonVoiceChanger/main.py"
+FULL_PATH="$(dirname "$0")/main.py"
+ACTIVATE_PATH="$(dirname "$0")/.venv/bin/activate"
 
 PID=$(ps -aux | grep '[m]ain.py' | awk '{print $2}' | head -n 1)
 
@@ -7,6 +8,7 @@ if [ -n "$PID" ]; then
     echo "Sending SIGTERM to process $PID"
     kill -TERM $PID
 else
-    nohup python3 "$FULL_PATH" >/dev/null 2>&1 &
-    echo "Python script not running."
+    source $ACTIVATE_PATH
+    nohup python3 $FULL_PATH >/dev/null 2>&1 &
+    echo "Start VoiceChanger"
 fi
