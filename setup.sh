631cfdc7afe8bbc7ccd82f495527258a95e53816
xmasterpiecex <a211019971420125@gmail.com>
2025-08-30 20:41:35 +0300

    added some echo while setuping

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
+set -e
 
+echo "Creating virtual environment"
 python3 -m venv ./.venv
 
-source .venv/bin/activate
+echo "Activating virtual environment"
+source ".venv/bin/activate"
 
+echo "Installing dependencies"
+pip install --upgrade pip
 pip install -r requirement.txt
